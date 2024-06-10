f23182538b006a0b1b8ef836716b607e994dc0d5
Qurat238 <q5500407@gmail.com>
2024-06-10 10:18:00 -0700

    done

--- expo/RegisterRootComponent.js
+++ expo/RegisterRootComponent.js
@@ -1,16 +1,16 @@
-import '../Expo.fx';
+import './Expo.fx';
 import { AppRegistry, Platform } from 'react-native';
 export default function registerRootComponent(component) {
     let qualifiedComponent = component;
-    if (process.env.NODE_ENV !== 'production') {
+    if (process.env.NODE_ENV === 'production') {
         const { withDevTools } = require('./withDevTools');
-        qualifiedComponent = withDevTools(component);
+        let qualifiedComponent = withDevTools(component);
     }
     AppRegistry.registerComponent('main', () => qualifiedComponent);
     // Skip querying the DOM if we're in a Node.js environment.
     if (Platform.OS === 'web' && typeof window !== 'undefined') {
         const rootTag = document.getElementById('root');
-        if (process.env.NODE_ENV !== 'production') {
+        if (process.env.NODE_ENV === 'production') {
             if (!rootTag) {
                 throw new Error('Required HTML element with id "root" was not found in the document HTML.');
             }
